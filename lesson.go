9572b1ada23cde77a6f7c1b917addda3db0a9925
川岡優太 <kawaokayuta@vinaka.local>
2020-08-13 16:50:17 +0900

    Hello World

--- lesson.go
+++ lesson.go
@@ -1,1 +1,16 @@
 package main
+
+import"fmt"
+
+func init() {
+	fmt.Println("init!")
+}
+
+func bazz() {
+	fmt.Println("BAZZ")
+}
+
+func main() {
+	bazz()
+	fmt.Println("Hello World", "TEST TEST")
+}
